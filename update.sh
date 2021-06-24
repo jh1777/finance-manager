a32178047d2d9f40c0b0db93c29f892c2553d8f5
Joerg Hannemann <jh.stores1@gmail.com>
2021-06-24 19:08:08 +0200

    changes update sh

--- update.sh
+++ update.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# OLD:: pkill -f "ng serve --port 4567"
-
+echo "<<< Stop old docker container >>>" 
 docker stop finance-manager-container
+
+echo "<<< Remove old docker container >>>" 
 docker rm finance-manager-container
+
+echo "<<< Pull latest chnages from Git Repo >>>" 
 git pull
+
+echo "<<< Build the Angular Application >>>" 
 ng build --prod
+
+echo "<<< Build new docker image >>>" 
 docker build -t finance-manager .
+
+echo "<<< Run the new docker image as finance-manager-container >>>" 
 docker run --name finance-manager-container -d -p 4567:80 finance-manager
