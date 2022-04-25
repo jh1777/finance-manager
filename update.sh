859b597a19344e11060dc93349a9d89f37293de2
Joerg Hannemann <jh.stores1@gmail.com>
2022-04-25 19:06:15 +0200

    added restart policy

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ docker rm finance-manager-container
 git pull
 ng build --prod
 docker build -t finance-manager .
-docker run --name finance-manager-container -d -p 4567:80 finance-manager
+docker run --name finance-manager-container --restart=on-failure:10 -d -p 4567:80 finance-manager
