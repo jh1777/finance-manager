7ccdf8f1d214731ff77b399e1cea81dace1b3762
Joerg <jh.stores1@gmail.com>
2022-05-03 21:22:06 +0200

    migrated insurance and pension

--- src/app/pages/pension/pension.component.ts
+++ src/app/pages/pension/pension.component.ts
@@ -1,25 +1,25 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
 import { timer } from 'rxjs';
 import { getIconWithName } from 'src/app/data/iconFactory';
 import { ModalService } from 'src/app/modalModule';
 import { ApiService } from 'src/app/services/api.service';
-import { Absicherung } from 'src/app/services/models/absicherung';
 import { NavigationService } from 'src/app/services/navigation.service';
 import { NumberTableCell, StyledTextTableCell, TableHeader, TableRow, TableRowAction, TableSize, TextTableCell } from 'src/app/ui';
 import { SortEntry } from 'src/app/ui/models/table/sortEntry';
+import { FinanceApiService, Pension } from 'src/services/finance-api.service';
 
 @Component({
   selector: 'app-pension',
   templateUrl: './pension.component.html',
   styleUrls: ['./pension.component.scss']
 })
-export class PensionComponent implements OnInit {
+export class PensionComponent {
   public pageTitle = "Pension";
   public lastResult: string = '';
 
-  private data: Array<Absicherung> = [];
+  private data: Array<Pension> = [];
 
   /// Heder Navigation
   public addEntryIcon: string = getIconWithName('plus-circle-line');
@@ -33,8 +33,8 @@ export class PensionComponent implements OnInit {
   ///
 
   // Table Actions
-  public changeEntry = new Absicherung();
-  public deletionEntry: Absicherung;
+  public changeEntry = new Pension();
+  public deletionEntry: Pension;
   public deleteConfirmMessage: string;
   //
 
@@ -60,16 +60,12 @@ export class PensionComponent implements OnInit {
     private modalService: ModalService,
     private navigationService: NavigationService,
     private datePipe: DatePipe,
+    private financeApi: FinanceApiService,
     private currencyPipe: CurrencyPipe
   ) {
 
     this.navigationService.activeMenu.next(5);
-    this.api.setService("pensions");
     this.loadData();
-
-  }
-  ngOnInit(): void {
-
   }
 
   private showResultWithTimer(message: string) {
@@ -79,7 +75,21 @@ export class PensionComponent implements OnInit {
   }
 
   private loadData() {
-    this.api.getEntries<Absicherung>().subscribe({
+    this.financeApi.getPensions().subscribe({
+      next: (result) => {
+
+        this.data = result.SortAscending('name');
+        // Person filter
+        this.data = this.data.filter(d => d.person == this.currentPerson);
+        //  Mapping
+        this.mapDataToTableModel();
+      },
+      error: (err) => {
+        console.log("Error loading pensions!", err);
+      }
+    });
+    /*
+    this.api.getEntries<Pension>().subscribe({
       next: (result) => {
 
         this.data = result.SortAscending('Name');
@@ -92,6 +102,7 @@ export class PensionComponent implements OnInit {
         console.log("Error loading pensions!", err);
       }
     })
+    */
   }
 
   private createHeader() {
@@ -138,19 +149,19 @@ export class PensionComponent implements OnInit {
       action.icon = getIconWithName("trash-line");
       action.action = (id: string) => {
         this.deletionEntry = entry;
-        this.deleteConfirmMessage = `Confirm Entry deletion: Id=${entry._id}: ${entry.Name}?`;
+        this.deleteConfirmMessage = `Confirm Entry deletion: Id=${entry.id}: ${entry.name}?`;
         this.openModal('delete-confirmation');
       };
       row.actions.push(action);
 
       // Cells
-      row.cells.push(new TextTableCell({ id: entry._id, label: entry._id ? `${entry._id}` : "n/a" }));
-      row.cells.push(new StyledTextTableCell({ id: entry._id, label: entry.Name, style: { 'font-weight': '500' } }));
-      row.cells.push(new TextTableCell({ id: entry._id, label: entry.Versicherung }));
-      row.cells.push(new TextTableCell({ id: entry._id, label: entry.Versicherungsnummer }));
+      row.cells.push(new TextTableCell({ id: entry.id, label: entry.id ? `${entry.id}` : "n/a" }));
+      row.cells.push(new StyledTextTableCell({ id: entry.id, label: entry.name, style: { 'font-weight': '500' } }));
+      row.cells.push(new TextTableCell({ id: entry.id, label: entry.versicherung }));
+      row.cells.push(new TextTableCell({ id: entry.id, label: entry.versicherungsnummer }));
 
       // Cells
-      let cell = new NumberTableCell({ id: entry._id, label: this.currencyPipe.transform(entry.Monatsbetrag), numericValue: entry.Monatsbetrag });
+      let cell = new NumberTableCell({ id: entry.id, label: this.currencyPipe.transform(entry.monatsbetrag), numericValue: entry.monatsbetrag });
       cell.action = () => {
         this.changeEntry = entry;
         this.openModal('change-value');
@@ -158,7 +169,7 @@ export class PensionComponent implements OnInit {
       cell.actionIcon = getIconWithName("slider-line");
       row.cells.push(cell);
 
-      let cell2 = new NumberTableCell({ id: entry._id, label: this.currencyPipe.transform(entry.Einmalzahlung), numericValue: entry.Einmalzahlung });
+      let cell2 = new NumberTableCell({ id: entry.id, label: this.currencyPipe.transform(entry.einmalzahlung), numericValue: entry.einmalzahlung });
       cell2.action = () => {
         this.changeEntry = entry;
         this.openModal('change-value');
@@ -166,7 +177,7 @@ export class PensionComponent implements OnInit {
       cell2.actionIcon = getIconWithName("slider-line");
       row.cells.push(cell2);
 
-      let cell3 = new NumberTableCell({ id: entry._id, label: this.currencyPipe.transform(entry.Todesfallsumme), numericValue: entry.Todesfallsumme });
+      let cell3 = new NumberTableCell({ id: entry.id, label: this.currencyPipe.transform(entry.todesfallsumme), numericValue: entry.todesfallsumme });
       cell3.action = () => {
         this.changeEntry = entry;
         this.openModal('change-value');
@@ -174,7 +185,7 @@ export class PensionComponent implements OnInit {
       cell3.actionIcon = getIconWithName("slider-line");
       row.cells.push(cell3);
 
-      let cell4 = new NumberTableCell({ id: entry._id, label: this.currencyPipe.transform(entry.Berufsunfaehigkeit), numericValue: entry.Berufsunfaehigkeit });
+      let cell4 = new NumberTableCell({ id: entry.id, label: this.currencyPipe.transform(entry.berufsunfaehigkeit), numericValue: entry.berufsunfaehigkeit });
       cell4.action = () => {
         this.changeEntry = entry;
         this.openModal('change-value');
@@ -182,8 +193,8 @@ export class PensionComponent implements OnInit {
       cell4.actionIcon = getIconWithName("slider-line");
       row.cells.push(cell4);
 
-      row.cells.push(new TextTableCell({ id: entry._id, label: this.datePipe.transform(entry.Faelligkeit) }));
-      row.cells.push(new TextTableCell({ id: entry._id, label: entry.Kommentar }));
+      row.cells.push(new TextTableCell({ id: entry.id, label: this.datePipe.transform(entry.faelligkeit) }));
+      row.cells.push(new TextTableCell({ id: entry.id, label: entry.kommentar }));
       result.push(row);
     });
 
@@ -210,19 +221,18 @@ export class PensionComponent implements OnInit {
   // Delete
   /**
    * Deletes one entry from the Database
-   * @param $event Absicherung
+   * @param $event Pension
    */
-  public deleteEntry($event: Absicherung) {
+  public deleteEntry($event: Pension) {
     if ($event) {
       // Call the API to delete the entry
-      this.api.setService("pensions");
-      this.api.deleteEntryById<Absicherung>($event._id).subscribe({
+      this.financeApi.deletePension($event.id).subscribe({
         next: (res) => {
-          this.showResultWithTimer(`Item ${$event._id}: ${$event.Name} Deletion: HTTP Code ${res.status} ${res.statusText}`);
+          this.showResultWithTimer(`Item ${$event.id}: ${$event.name} Deletion: Acknowledged=${res.isAcknowledged} DeletedCount=${res.deletedCount}`);
           this.loadData();
         },
         error: (err) => {
-          this.showResultWithTimer(`Item ${$event._id} Deletion Failed: ${err}`);
+          this.showResultWithTimer(`Item ${$event.id} Deletion Failed: ${err}`);
         }
       });
     }
@@ -235,12 +245,12 @@ export class PensionComponent implements OnInit {
    */
    public change() {
     // Create change partial item
-    let changedData: Partial<Absicherung> = {
-      Monatsbetrag: this.changeEntry.Monatsbetrag,
-      Einmalzahlung: this.changeEntry.Einmalzahlung
+    let changedData: Partial<Pension> = {
+      monatsbetrag: this.changeEntry.monatsbetrag,
+      einmalzahlung: this.changeEntry.einmalzahlung
     }
 
-    this.changeItem(this.changeEntry._id, changedData, true);
+    this.changeItem(this.changeEntry.id, changedData, true);
     this.closeModal('change-value');
   }
 
@@ -249,28 +259,25 @@ export class PensionComponent implements OnInit {
    * @param id number Item id to change
    * @param item Partial<Ausgabe>
    */
-  private changeItem(id: string, item: Partial<Absicherung>, reload: boolean = false) {
-
-    this.api.setService("pensions");
-    this.api.changeEntry<Partial<Absicherung>>(id, item).subscribe(
-        res => {
-          var response = <HttpResponse<Partial<Absicherung>>>res;
-          this.showResultWithTimer(`PUT Absicherung item: ${this.changeEntry.Name}/${this.changeEntry._id}: HTTP Code ${response.status}`);
-          if (reload) {
-            this.loadData();
-          }
-        },
-        (err: HttpErrorResponse) => {
-          this.showResultWithTimer(`Error changing the pension entry!: ${err}`);
-          console.error(`Error changing the pension entry!: ${err}`);
+  private changeItem(id: string, item: Partial<Pension>, reload: boolean = false) {
+    this.financeApi.updatePension(id, item as Pension).subscribe(
+      res => {
+        this.showResultWithTimer(`PUT Pension item: ${this.changeEntry.name}/${this.changeEntry.id}: Acknowledged=${res.isAcknowledged} ModifiedCount=${res.modifiedCount}`);
+        if (reload) {
+          this.loadData();
         }
-      );
+      },
+      (err: HttpErrorResponse) => {
+        this.showResultWithTimer(`Error changing the pension entry!: ${err}`);
+        console.error(`Error changing the pension entry!: ${err}`);
+      }
+    );
   }
   // -----
 
   // Create
   public newEntry() {
-    this.changeEntry = new Absicherung();
+    this.changeEntry = new Pension();
     this.openModal('change-entry');
   }
 
@@ -278,33 +285,26 @@ export class PensionComponent implements OnInit {
    * Create new Item in Database
    * @param item Ausgabe
    */
-   private createItem(item: Absicherung) {
-     if (item.Faelligkeit) {
-       item.Faelligkeit = new Date(item.Faelligkeit);
+   private createItem(item: Pension) {
+     if (item.faelligkeit) {
+       item.faelligkeit = new Date(item.faelligkeit);
      }
 
-    item.Person = this.currentPerson;
-
-    this.api.setService("pensions");
-    this.api.createEntry<Absicherung>([item]).subscribe(
-        res => {
-          var response = <HttpResponse<Array<Absicherung>>>res;
-          this.showResultWithTimer(`POST Absicherung item: ${item.Name}: HTTP Code ${response.status}`);
-
-          if (res.ok) {
-            this.loadData();
-          }
-        },
-        (err: HttpErrorResponse) => {
-          this.showResultWithTimer(`Error creating the pension entry!: ${err}`);
-        }
-      );
+    item.person = this.currentPerson;
+    this.financeApi.createPension(item).subscribe(
+      res => {
+        this.showResultWithTimer(`POST Pension item: ${item.name}: Result=${JSON.stringify(res)}`);
+        this.loadData();
+      },
+      (err: HttpErrorResponse) => {
+        this.showResultWithTimer(`Error creating the pension entry!: ${err}`);
+      }
+    );
   }
 
-
   public changeOrCreateEntry() {
-    if (this.changeEntry._id) {
-      this.changeItem(this.changeEntry._id, this.changeEntry,true);
+    if (this.changeEntry.id) {
+      this.changeItem(this.changeEntry.id, this.changeEntry,true);
     } else {
       // New Entry
       this.createItem(this.changeEntry);
